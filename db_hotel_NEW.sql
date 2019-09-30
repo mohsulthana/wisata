7fb5c84f9712f97b99d514b0a9f6c6d55d809b04
Mohammad Sulthan <muhammadsultana@outlook.com>
2019-09-30 14:00:17 +0700

    new db

--- db_hotel_NEW.sql
+++ db_hotel_NEW.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2019 at 04:41 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Waktu pembuatan: 30 Sep 2019 pada 08.59
+-- Versi server: 10.4.6-MariaDB
+-- Versi PHP: 7.1.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `acara`
+-- Struktur dari tabel `acara`
 --
 
 CREATE TABLE `acara` (
@@ -36,7 +36,7 @@ CREATE TABLE `acara` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `acara`
+-- Dumping data untuk tabel `acara`
 --
 
 INSERT INTO `acara` (`id_acara`, `nama_acara`, `tanggal`, `lokasi_acara`) VALUES
@@ -45,7 +45,7 @@ INSERT INTO `acara` (`id_acara`, `nama_acara`, `tanggal`, `lokasi_acara`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -57,7 +57,7 @@ CREATE TABLE `user` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wisata`
+-- Struktur dari tabel `wisata`
 --
 
 CREATE TABLE `wisata` (
@@ -66,68 +66,68 @@ CREATE TABLE `wisata` (
   `alamat` text NOT NULL,
   `jenis` varchar(20) NOT NULL,
   `kabupaten` varchar(50) NOT NULL,
-  `deskripsi` text NOT NULL
+  `deskripsi` text NOT NULL,
+  `foto` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `wisata`
+-- Dumping data untuk tabel `wisata`
 --
 
-INSERT INTO `wisata` (`id`, `nama`, `alamat`, `jenis`, `kabupaten`, `deskripsi`) VALUES
-(1, 'Taman Purbakala', 'Gandus', 'Alam', 'palembang', ''),
-(2, 'Perkampungan Nelayan Sungsang', 'Sungsang, Banyuasin II', 'Buatan', 'Banyuasin', ''),
-(3, 'Kawasan Benteng Kuto Besak ', 'Jln. SMB II, Palembang', 'Sejarah', 'Palembang', ''),
-(4, 'Makam Putri Pinang Masak', 'Camat Tg. Batu', 'Wisata Religi', 'Ogan Ilir', ''),
-(5, 'Danau Teluk Gelam', 'Kec. Teluk Gelam ', 'Alam', 'Ogan Komering Ilir', ''),
-(6, 'Kuala Lebung Itam', 'Kec. Tulung Selapan, OKI ', 'Alam ', 'Ogan Komering Ilir', ''),
-(7, 'Danau Deduhok', 'Dusun IV Rantau Dedap, Desa Segamit, Kec. Semende Darat ', 'Alam', 'Muaraenim', ''),
-(8, 'Gunung Seminung', 'Desa Kota Batu. Kec. Banding Agung OKU Selatan', 'Alam', 'Ogankomeringuluselatan', ''),
-(9, 'Lebung Karangan', 'Tj.  Senai', 'Alam', 'oganilir', ''),
-(13, 'w', 'w', 'w', 'w', 'w');
+INSERT INTO `wisata` (`id`, `nama`, `alamat`, `jenis`, `kabupaten`, `deskripsi`, `foto`) VALUES
+(1, 'Taman Purbakala', 'Gandus', 'Alam', 'palembang', '', ''),
+(2, 'Perkampungan Nelayan Sungsang', 'Sungsang, Banyuasin II', 'Buatan', 'Banyuasin', '', ''),
+(3, 'Kawasan Benteng Kuto Besak ', 'Jln. SMB II, Palembang', 'Sejarah', 'Palembang', '', ''),
+(4, 'Makam Putri Pinang Masak', 'Camat Tg. Batu', 'Wisata Religi', 'Ogan Ilir', '', ''),
+(5, 'Danau Teluk Gelam', 'Kec. Teluk Gelam ', 'Alam', 'Ogan Komering Ilir', '', ''),
+(6, 'Kuala Lebung Itam', 'Kec. Tulung Selapan, OKI ', 'Alam ', 'Ogan Komering Ilir', '', ''),
+(7, 'Danau Deduhok', 'Dusun IV Rantau Dedap, Desa Segamit, Kec. Semende Darat ', 'Alam', 'Muaraenim', '', ''),
+(8, 'Gunung Seminung', 'Desa Kota Batu. Kec. Banding Agung OKU Selatan', 'Alam', 'Ogankomeringuluselatan', '', ''),
+(9, 'Lebung Karangan', 'Tj.  Senai', 'Alam', 'oganilir', '', '');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `acara`
+-- Indeks untuk tabel `acara`
 --
 ALTER TABLE `acara`
   ADD PRIMARY KEY (`id_acara`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `wisata`
+-- Indeks untuk tabel `wisata`
 --
 ALTER TABLE `wisata`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `acara`
+-- AUTO_INCREMENT untuk tabel `acara`
 --
 ALTER TABLE `acara`
   MODIFY `id_acara` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `wisata`
+-- AUTO_INCREMENT untuk tabel `wisata`
 --
 ALTER TABLE `wisata`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
